430fc4fd89ac572aacf0b4b8dfbb8c598beb1d06
aam-himel <aam.himel@gmail.com>
2022-04-24 14:09:02 +0600

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+});
+export default App;
